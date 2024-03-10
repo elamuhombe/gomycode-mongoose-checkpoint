9421d65624ad86bccd74f5086a3ca503841217b8
Elaine Muhombe <emuhombe@gmail.com>
2024-03-10 18:38:16 +0300

    chore: Add code to connect to MongoDB database

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 // Import required modules
 const express = require("express"); // Express framework
+const mongoose = require("mongoose")// Mongoose
 require("dotenv").config(); // Load environment variables
 
 // Create Express app
 const app = express();
 const PORT = 5000; // Port for server to listen on
+const MONGO_URI = process.env.MONGO_URI; // MongoDB connection URI
 
-
+// Connect to MongoDB database
+async function connectToDatabase() {
+    try {
+      await mongoose.connect(MONGO_URI);
+      console.log("Connected to database");
+    } catch (error) {
+      console.error("Error connecting to database", error.message);
+    }
+  }
+  connectToDatabase();
+  
 // Start the Express server
 app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`);
