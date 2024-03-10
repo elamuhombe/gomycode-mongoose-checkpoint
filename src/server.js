997d4de369e1952333e043287352b35ee90d1793
Elaine Muhombe <emuhombe@gmail.com>
2024-03-10 18:32:24 +0300

    chore: Create an instance of express server and start the server

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require ('dotenv').config();
+// Import required modules
+const express = require("express"); // Express framework
+require("dotenv").config(); // Load environment variables
+
+// Create Express app
 const app = express();
-const PORT = 5000
+const PORT = 5000; // Port for server to listen on
 
 
-const MONGO_URI = process.env.MONGO_URI;
-
-async function connectToDatabase(){
-    try{
-        await mongoose.connect(MONGO_URI);
-        console.log('Connected to database');
-    }
-    catch(error){
-        console.error('Error connecting to database', error.message)
-    }
-}
-connectToDatabase();
-app.listen(PORT, ()=>{
+// Start the Express server
+app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`);
-})
+  });
+  
