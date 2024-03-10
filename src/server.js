033e696ae3f3d770f2ba70c04abe250ff33cfc28
Elaine Muhombe <emuhombe@gmail.com>
2024-03-10 18:46:42 +0300

    chore: Create an Array of People data and create several people using the array

--- src/server.js
+++ src/server.js
@@ -9,6 +9,38 @@ const app = express();
 const PORT = 5000; // Port for server to listen on
 const MONGO_URI = process.env.MONGO_URI; // MongoDB connection URI
 
+// Array of people data
+const arrayOfPeople = [
+    { name: "Karen Michaels", age: 20, favoriteFoods: ["Fries", "Chicken"] },
+    {
+      name: "Ibrahim Gates",
+      age: 26,
+      favoriteFoods: ["Potatoes", "Fish", "Broccoli"],
+    },
+    { name: "David Nelson", age: 34, favoriteFoods: ["Shrimp", "Rice"] },
+    { name: "Brian Michaels", age: 20, favoriteFoods: ["Burger", "eggs"] },
+    {
+      name: "Wilson Daniels",
+      age: 28,
+      favoriteFoods: ["Rice", "Chicken", "Peas"],
+    },
+    { name: "Larry Anthony", age: 36, favoriteFoods: ["Potatoes", "Beef"] },
+    { name: "Wendy Sandra", age: 39, favoriteFoods: ["Crab", "Fries"] },
+    { name: "Sam Michaels", age: 23, favoriteFoods: ["Bananas", "Beef"] },
+    { name: "Karen Michaels", age: 32, favoriteFoods: ["Oyster", "Bananas"] },
+    { name: "Isaac Shah", age: 35, favoriteFoods: ["Chicken", "Pasta", "Kales"] },
+    { name: "Esther Hillary", age: 26, favoriteFoods: ["Sphagetti", "Beef"] },
+    {
+      name: "Cole Keith",
+      age: 37,
+      favoriteFoods: ["Rice", "Chicken", "Spinach"],
+    },
+    { name: "Cindy Joel", age: 23, favoriteFoods: ["Apple Pie", "Yoghurt"] },
+    { name: "Bruce Jackson", age: 33, favoriteFoods: ["Bacon", "Fish", "Rice"] },
+    { name: "David Nelson", age: 34, favoriteFoods: ["Tomato Pie", "Chicken"] },
+    { name: "Rita Rose", age: 29, favoriteFoods: ["Burger", "Fries"] },
+  ];
+
 // Connect to MongoDB database
 async function connectToDatabase() {
     try {
@@ -35,6 +67,15 @@ person1
   .catch((err) => {
     console.error(err);
   });
+// Create several people with Model.create(), using the arrayOfPeople data
+Person.create(arrayOfPeople)
+.then(createdPeople => {
+    console.log('Several people created:', createdPeople);
+})
+.catch(err => {
+    console.error(err);
+});
+  
   
 // Start the Express server
 app.listen(PORT, () => {
