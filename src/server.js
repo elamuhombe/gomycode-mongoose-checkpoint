a50f502e7019af9f3033eb381150ac353cda54e0
Elaine Muhombe <emuhombe@gmail.com>
2024-03-11 20:34:09 +0300

    Format code

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // Import required modules
 const express = require("express"); // Express framework
-const mongoose = require("mongoose")// Mongoose
-const Person = require('./models/Person'); // Person model schema
+const mongoose = require("mongoose"); // Mongoose
+const Person = require("./models/Person"); // Person model schema
 require("dotenv").config(); // Load environment variables
 
 // Create Express app
@@ -11,53 +11,69 @@ const MONGO_URI = process.env.MONGO_URI; // MongoDB connection URI
 
 // Array of people data
 const arrayOfPeople = [
-    { name: "Karen Michaels", age: 20, favoriteFoods: ["fries", "chicken", "burritos"] },
-    {
-      name: "Ibrahim Gates",
-      age: 26,
-      favoriteFoods: ["potatoes", "fish", "broccoli"],
-    },
-    { name: "David Nelson", age: 34, favoriteFoods: ["shrimp", "rice", "chicken"] },
-    { name: "Brian Michaels", age: 20, favoriteFoods: ["hamburger", "eggs"] },
-    {
-      name: "Wilson Daniels",
-      age: 28,
-      favoriteFoods: ["rice", "chicken", "peas", "burritos"],
-    },
-    { name: "Larry Anthony", age: 36, favoriteFoods: ["potatoes", "beef", "burritos"] },
-    { name: "Wendy Sandra", age: 39, favoriteFoods: ["Crab", "fries"] },
-    { name: "Sam Michaels", age: 23, favoriteFoods: ["bananas", "beef"] },
-    { name: "Karen Michaels", age: 32, favoriteFoods: ["oyster", "bananas", "chicken"] },
-    { name: "Isaac Shah", age: 35, favoriteFoods: ["chicken", "pasta", "kales"] },
-    { name: "Esther Hillary", age: 26, favoriteFoods: ["sphagetti", "beef"] },
-    {
-      name: "Cole Keith",
-      age: 37,
-      favoriteFoods: ["rice", "chicken", "spinach", "burritos"],
-    },
-    { name: "Cindy Joel", age: 23, favoriteFoods: ["apple pie", "yoghurt"] },
-    { name: "Bruce Jackson", age: 33, favoriteFoods: ["bacon", "fish", "rice"] },
-    { name: "David Nelson", age: 34, favoriteFoods: ["tomato pie", "chicken"] },
-    { name: "Rita Rose", age: 29, favoriteFoods: ["hamburger", "fries"] },
-  ];
+  {
+    name: "Karen Michaels",
+    age: 20,
+    favoriteFoods: ["fries", "chicken", "burritos"],
+  },
+  {
+    name: "Ibrahim Gates",
+    age: 26,
+    favoriteFoods: ["potatoes", "fish", "broccoli"],
+  },
+  {
+    name: "David Nelson",
+    age: 34,
+    favoriteFoods: ["shrimp", "rice", "chicken"],
+  },
+  { name: "Brian Michaels", age: 20, favoriteFoods: ["hamburger", "eggs"] },
+  {
+    name: "Wilson Daniels",
+    age: 28,
+    favoriteFoods: ["rice", "chicken", "peas", "burritos"],
+  },
+  {
+    name: "Larry Anthony",
+    age: 36,
+    favoriteFoods: ["potatoes", "beef", "burritos"],
+  },
+  { name: "Wendy Sandra", age: 39, favoriteFoods: ["Crab", "fries"] },
+  { name: "Sam Michaels", age: 23, favoriteFoods: ["bananas", "beef"] },
+  {
+    name: "Karen Michaels",
+    age: 32,
+    favoriteFoods: ["oyster", "bananas", "chicken"],
+  },
+  { name: "Isaac Shah", age: 35, favoriteFoods: ["chicken", "pasta", "kales"] },
+  { name: "Esther Hillary", age: 26, favoriteFoods: ["sphagetti", "beef"] },
+  {
+    name: "Cole Keith",
+    age: 37,
+    favoriteFoods: ["rice", "chicken", "spinach", "burritos"],
+  },
+  { name: "Cindy Joel", age: 23, favoriteFoods: ["apple pie", "yoghurt"] },
+  { name: "Bruce Jackson", age: 33, favoriteFoods: ["bacon", "fish", "rice"] },
+  { name: "David Nelson", age: 34, favoriteFoods: ["tomato pie", "chicken"] },
+  { name: "Rita Rose", age: 29, favoriteFoods: ["hamburger", "fries"] },
+];
 
 // Connect to MongoDB database
 async function connectToDatabase() {
-    try {
-      await mongoose.connect(MONGO_URI);
-      console.log("Connected to database");
-    } catch (error) {
-      console.error("Error connecting to database", error.message);
-    }
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Connected to database");
+  } catch (error) {
+    console.error("Error connecting to database", error.message);
   }
-  connectToDatabase();
+}
+connectToDatabase();
 
 // Create and Save a Record of a Model
 const person1 = new Person({
-    name: "John Doe",
-    age: 33,
-    favoriteFoods: ["pizza", "pasta"],
-  });
+  name: "John Doe",
+  age: 33,
+  favoriteFoods: ["pizza", "pasta"],
+});
 
 person1
   .save()
@@ -69,21 +85,21 @@ person1
   });
 // Create several people with Model.create(), using the arrayOfPeople data
 Person.create(arrayOfPeople)
-.then(createdPeople => {
-    console.log('Several people created:', createdPeople);
-})
-.catch(err => {
+  .then((createdPeople) => {
+    console.log("Several people created:", createdPeople);
+  })
+  .catch((err) => {
     console.error(err);
-});
-  
- // Find distinct names containing "Michaels"
+  });
+
+// Find distinct names containing "Michaels"
 Person.distinct("name", { name: /Michaels/ })
-.then((people) => {
-  console.log('Unique names containing "Michaels":', people);
-})
-.catch((err) => {
-  console.error(err);
-});
+  .then((people) => {
+    console.log('Unique names containing "Michaels":', people);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 const food = "Fries"; // Example food to search for
 
 // Find a person with the specified food in their favorites
@@ -98,28 +114,28 @@ Person.findOne({ favoriteFoods: food })
   .catch((err) => {
     console.error(err);
   });
-  const personId = '65ef33e85c2acbeaecbcf332'; // Example personId to search for
+const personId = "65ef33e85c2acbeaecbcf332"; // Example personId to search for
 
-  // Validate the _id value
-  if (!mongoose.Types.ObjectId.isValid(personId)) {
-    console.log(`Invalid personId: ${personId}`);
-    return;
-  }
-  
-  // Find a person by id
-  Person.findById(personId)
-    .then((person) => {
-      if (person) {
-        console.log(`Person with id ${personId}:`, person);
-      } else {
-        console.log(`No person found with id ${personId}.`);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+// Validate the _id value
+if (!mongoose.Types.ObjectId.isValid(personId)) {
+  console.log(`Invalid personId: ${personId}`);
+  return;
+}
 
-  const personName = "Karen Michaels"; // Example personName to search for
+// Find a person by id
+Person.findById(personId)
+  .then((person) => {
+    if (person) {
+      console.log(`Person with id ${personId}:`, person);
+    } else {
+      console.log(`No person found with id ${personId}.`);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const personName = "Karen Michaels"; // Example personName to search for
 
 // Find a person by name and update their age
 Person.findOneAndUpdate(
@@ -138,7 +154,7 @@ Person.findOneAndUpdate(
     console.error("Error updating person:", err);
   });
 
-  // Async function for removing a person
+// Async function for removing a person
 async function main() {
   try {
     // Your existing code goes here
@@ -163,15 +179,15 @@ Person.deleteMany({ age: { $gt: 30 } })
     console.error("Error deleting documents:", err);
   });
 
- // Async function to fetch data
+// Async function to fetch data
 async function fetchData() {
   try {
     // Find documents where favoriteFoods contain 'burritos', sort by name, limit to 2, exclude age field
-    const data = await Person.find({ favoriteFoods: 'burritos' })
-                              .sort({ name: 1 })
-                              .limit(2)
-                              .select('-age')
-                              .exec();
+    const data = await Person.find({ favoriteFoods: "burritos" })
+      .sort({ name: 1 })
+      .limit(2)
+      .select("-age")
+      .exec();
     console.log(data); // Log the fetched data
   } catch (err) {
     console.error(err); // Log any errors that occur
@@ -180,12 +196,9 @@ async function fetchData() {
 
 fetchData(); // Invoke the fetchData function
 // Invoke main function
-main()
-  
-  
-
+main();
 
 // Start the Express server
 app.listen(PORT, () => {
-    console.log(`server is running at ${PORT}`);
-  });
+  console.log(`server is running at ${PORT}`);
+});
